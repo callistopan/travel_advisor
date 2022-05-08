017fbc2a349b1414ac84d68c2acd6c30bb3daf0d
callistopan <callistopan@gmail.com>
2022-05-08 10:03:10 +0530

    added map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React from "react"
+import GoogleMapReact from "google-map-react"
+import {Paper,Typography,useMediaQuery} from "@material-ui/core"
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined"
+import Rating from "@material-ui/lab"
 
+import useStyles from './styles'
 const Map=()=>{
+    const classes=useStyles()
+    const isMobile=useMediaQuery('(min-width:600px)')
+    const coordinates={ lat:0, lng:0} //default coordinates
     return (
-        <h1>Map</h1>
+        <><div  className={classes.mapContainer}>
+            <GoogleMapReact
+
+
+                bootstrapURLKeys={{key:'AIzaSyA44Y19HFz0YfM1XqIQZI-l2Ksn_JBtryE'}}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50,50,50,50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+
+
+
+            
+            
+            
+            
+            >
+            </GoogleMapReact>
+
+
+        </div>
+        </>
+        
+        
+
     )
 }
-
-export default Map;
+export default Map
